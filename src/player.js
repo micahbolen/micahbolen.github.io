831415e77f460c84f41a1a8dc9644b2098407610
Micah Bolen <micah.bolen@gmail.com>
2015-12-26 01:56:49 -0600

    Add video advertisement.

---
+++ src/player.js
@@ -0,0 +1,15 @@
+var player = videojs('content_video');
+
+var options = {
+  id: 'content_video',
+  adTagUrl: 'http://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-games-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on'
+};
+
+player.ima(options);
+player.ima.requestAds();
+// On mobile devices, you must call initializeAdDisplayContainer as the result
+// of a user action (e.g. button click). If you do not make this call, the SDK
+// will make it for you, but not as the result of a user action. For more info
+// see our examples, all of which are set up to work on mobile devices.
+// player.ima.initializeAdDisplayContainer();
+player.play();
